3738984145951e05774a6d32c45417619e096187
Salwa Al-Kuwari <susu@MacBook-Pro-Salwa.local>
2025-10-30 21:27:19 +0300

    Move dummy test to correct directory for Foundry CI

---
+++ test/Dummy.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+
+contract DummyTest is Test {
+    function test_dummy() public {
+        assertTrue(true);
+    }
+}
